0037ea5ad4bfc49faf249a1ec3e416fb73298b50
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-03-07 15:44:15 -0600

    Enhances UI.

--- SoonerFootballScores/SoonerFootballScores/UI/Season Games List/Custom Views/SeasonGameTableViewCell.swift
+++ SoonerFootballScores/SoonerFootballScores/UI/Season Games List/Custom Views/SeasonGameTableViewCell.swift
@@ -23,5 +23,25 @@ class SeasonGameTableViewCell: UITableViewCell {
         awayTeamScoreLabel.text = "\(game.awayTeamFinalScore)"
         homeTeamNameLabel.text = game.homeTeamName
         homeTeamScoreLabel.text = "\(game.homeTeamFinalScore)"
+        
+        if game.awayTeamName == "Oklahoma" && game.awayTeamFinalScore > game.homeTeamFinalScore {
+            formatWiningText()
+        } else if game.homeTeamName == "Oklahoma" && game.homeTeamFinalScore > game.awayTeamFinalScore {
+            formatWiningText()
+        } else {
+            formatLosingText()
+        }
+    }
+    
+    
+    func formatWiningText() {
+        soonerWinStatusLabel.text = "W"
+        soonerWinStatusLabel.textColor = .green
+    }
+    
+    
+    func formatLosingText() {
+        soonerWinStatusLabel.text = "L"
+        soonerWinStatusLabel.textColor = .red
     }
 }
